4e78b3356e9d8eff6a89dcc1402f9a97c1d9eb4c
Damien Whitten <dw@chaos404.com>
2024-01-20 12:30:40 -0800

    Split out CLI

--- cmd/main/server.go
+++ cmd/main/server.go
@@ -9,13 +9,17 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	sq "github.com/elgris/sqrl"
+	"github.com/google/uuid"
 	"github.com/pentops/log.go/grpc_log"
 	"github.com/pentops/log.go/log"
+	"github.com/pentops/o5-deploy-aws/app"
 	"github.com/pentops/o5-deploy-aws/awsinfra"
 	"github.com/pentops/o5-deploy-aws/deployer"
 	"github.com/pentops/o5-deploy-aws/github"
+	"github.com/pentops/o5-deploy-aws/localrun"
 	"github.com/pentops/o5-deploy-aws/protoread"
 	"github.com/pentops/o5-deploy-aws/service"
+	"github.com/pentops/o5-go/application/v1/application_pb"
 	"github.com/pentops/o5-go/deployer/v1/deployer_spb"
 	"github.com/pentops/o5-go/deployer/v1/deployer_tpb"
 	"github.com/pentops/o5-go/environment/v1/environment_pb"
@@ -38,6 +42,7 @@ func main() {
 
 	cmdGroup.Add("serve", commander.NewCommand(runServe))
 	cmdGroup.Add("migrate", commander.NewCommand(runMigrate))
+	cmdGroup.Add("local-deploy", commander.NewCommand(runLocalDeploy))
 
 	cmdGroup.RunMain("o5-deploy-aws", Version)
 }
@@ -213,4 +218,113 @@ func (rl RefLookup) PushTargets(push *github_pb.PushMessage) []string {
 		environments = append(environments, r.Targets...)
 	}
 	return environments
+}
+
+func runLocalDeploy(ctx context.Context, cfg struct {
+	EnvFilename   string `flag:"env" description:"environment file"`
+	AppFilename   string `flag:"app" description:"application file"`
+	Version       string `flag:"version" description:"version tag"`
+	DryRun        bool   `flag:"dry" description:"dry run - print template and exit"`
+	RotateSecrets bool   `flag:"rotate-secrets" description:"rotate secrets - rotate any existing secrets (e.g. db creds)"`
+	CancelUpdate  bool   `flag:"cancel-update" description:"cancel update - cancel any ongoing update prior to deployment"`
+	ScratchBucket string `flag:"scratch-bucket" env:"O5_DEPLOYER_SCRATCH_BUCKET" description:"An S3 bucket name to upload templates"`
+	QuickMode     bool   `flag:"quick" description:"Skips scale down/up, calls stack update with all changes once, and skips DB migration"`
+}) error {
+
+	if cfg.AppFilename == "" {
+		return fmt.Errorf("missing application file (-app)")
+	}
+
+	if cfg.ScratchBucket == "" {
+		return fmt.Errorf("missing scratch bucket (-scratch-bucket)")
+	}
+
+	if !cfg.DryRun {
+		if cfg.EnvFilename == "" {
+			return fmt.Errorf("missing environment file (-env)")
+		}
+
+		if cfg.Version == "" {
+			return fmt.Errorf("missing version (-version)")
+		}
+	}
+
+	awsConfig, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to load configuration: %w", err)
+	}
+
+	s3Client := s3.NewFromConfig(awsConfig)
+
+	appConfig := &application_pb.Application{}
+	if err := protoread.PullAndParse(ctx, s3Client, cfg.AppFilename, appConfig); err != nil {
+		return err
+	}
+
+	if appConfig.DeploymentConfig == nil {
+		appConfig.DeploymentConfig = &application_pb.DeploymentConfig{}
+	}
+
+	app, err := app.BuildApplication(appConfig, cfg.Version)
+	if err != nil {
+		return err
+	}
+	built := app.Build()
+
+	if cfg.DryRun {
+		tpl := built.Template
+		yaml, err := tpl.YAML()
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(yaml))
+
+		fmt.Println("-----")
+
+		for _, target := range built.SNSTopics {
+			fmt.Printf("SNS Topic: %s\n", target.Name)
+		}
+		return nil
+	}
+
+	env := &environment_pb.Environment{}
+	if err := protoread.PullAndParse(ctx, s3Client, cfg.EnvFilename, env); err != nil {
+		return err
+	}
+
+	awsTarget := env.GetAws()
+	if awsTarget == nil {
+		return fmt.Errorf("AWS Deployer requires the type of environment provider to be AWS")
+	}
+
+	clientSet := &awsinfra.ClientSet{
+		AssumeRoleARN: awsTarget.O5DeployerAssumeRole,
+		AWSConfig:     awsConfig,
+	}
+
+	awsRunner := localrun.NewInfraAdapter(clientSet)
+	stateStore := localrun.NewStateStore()
+	deploymentManager, err := deployer.NewSpecBuilder(stateStore, cfg.ScratchBucket, s3Client)
+	if err != nil {
+		return err
+	}
+	deploymentManager.RotateSecrets = cfg.RotateSecrets
+	deploymentManager.CancelUpdates = cfg.CancelUpdate
+	deploymentManager.QuickMode = cfg.QuickMode
+
+	eventLoop := localrun.NewEventLoop(awsRunner, stateStore, deploymentManager)
+
+	if err := stateStore.AddEnvironment(env); err != nil {
+		return err
+	}
+
+	trigger := &deployer_tpb.RequestDeploymentMessage{
+		DeploymentId:    uuid.NewString(),
+		Application:     appConfig,
+		Version:         cfg.Version,
+		EnvironmentName: env.FullName,
+	}
+
+	return eventLoop.Run(ctx, trigger)
+
 }
