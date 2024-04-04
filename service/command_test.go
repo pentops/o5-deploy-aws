7b14c4dda211f642ea3cd9ffaabbe16f98ead32b
Damien Whitten <dw@chaos404.com>
2024-04-05 10:49:31 +1100

    Upsert stack config breaks the status

--- service/command_test.go
+++ service/command_test.go
@@ -14,7 +14,77 @@ import (
 	"github.com/pentops/o5-go/environment/v1/environment_pb"
 	"github.com/pentops/outbox.pg.go/outboxtest"
 	"github.com/pentops/pgtest.go/pgtest"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+func assertCodeError(t *testing.T, err error, code codes.Code) {
+	t.Helper()
+	if err == nil {
+		t.Fatalf("expected error %s, got nil", code)
+	}
+	codeErr, ok := status.FromError(err)
+	if !ok {
+		t.Fatalf("expected error %s, got %v", code, err)
+	}
+	if codeErr.Code() != code {
+		t.Fatalf("expected error %s, got %s", code, codeErr.Code())
+	}
+}
+
+func TestConfiguration(t *testing.T) {
+
+	stateMachines, err := states.NewStateMachines()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	githubMock := mocks.NewGithub()
+
+	conn := pgtest.GetTestDB(t,
+		pgtest.WithDir("../ext/db"),
+		pgtest.WithSchemaName("testservice"),
+	)
+	//outbox := outboxtest.NewOutboxAsserter(t, conn)
+
+	ds, err := NewCommandService(conn, githubMock, stateMachines)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	{ // Attempt to upsert stack before the environment exists.
+		_, err := ds.UpsertStack(ctx, &deployer_spb.UpsertStackRequest{
+			StackId: "test-app",
+			Config:  &deployer_pb.StackConfig{},
+		})
+		assertCodeError(t, err, codes.NotFound)
+	}
+
+	_, err = ds.UpsertEnvironment(ctx, &deployer_spb.UpsertEnvironmentRequest{
+		Src: &deployer_spb.UpsertEnvironmentRequest_Config{
+			Config: &environment_pb.Environment{
+				FullName: "test",
+			},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := ds.UpsertStack(ctx, &deployer_spb.UpsertStackRequest{
+		StackId: "test-app",
+		Config:  &deployer_pb.StackConfig{},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.State.Status != deployer_pb.StackStatus_AVAILABLE {
+		t.Errorf("expected status %s, got %s", deployer_pb.StackStatus_AVAILABLE, res.State.Status)
+	}
+
+}
 
 func TestTriggerDeployment(t *testing.T) {
 
