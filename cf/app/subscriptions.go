0a021003abf8ea65075420fe196cd1bc8d46023b
Damien Whitten <dw@chaos404.com>
2024-06-10 23:44:40 -0700

    Map and test subscriptions

---
+++ cf/app/subscriptions.go
@@ -0,0 +1,214 @@
+package app
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/awslabs/goformation/v7/cloudformation"
+	"github.com/pentops/o5-deploy-aws/cf"
+	"github.com/pentops/o5-deploy-aws/gen/o5/awsdeployer/v1/awsdeployer_pb"
+	"github.com/pentops/o5-go/application/v1/application_pb"
+)
+
+type subscriptionPlan struct {
+	ingressEndpoints map[string]struct{}
+
+	parameters []*awsdeployer_pb.Parameter
+
+	eventBusSubscriptions []eventBusSubscriptionPlan
+	snsSubscriptions      []snsSubscriptionPlan
+}
+
+type eventBusSubscriptionPlan struct {
+	eventPattern busPattern
+	name         string
+}
+
+type eventBusRules struct {
+	topics        []string
+	services      []string
+	methods       []serviceAndMethod
+	sourceEnvRef  string
+	sourceEnvName string
+}
+
+type serviceAndMethod struct {
+	service string
+	method  string
+}
+
+type snsSubscriptionPlan struct {
+	topicARN string
+	name     string
+}
+
+type busPattern struct {
+	Detail busDetailPattern `json:"detail"`
+}
+
+type busDetailPattern struct {
+	// one of:
+	Or []singlePattern `json:"$or,omitempty"`
+	*singlePattern
+}
+
+type singlePattern struct {
+	DestinationTopic  []string `json:"destinationTopic,omitempty"`
+	GRPCService       []string `json:"grpcService,omitempty"`
+	GRPCMethod        []string `json:"grpcMethod,omitempty"`
+	SourceEnvironment []string `json:"sourceEnvironment"`
+}
+
+func buildSubscriptionPlan(spec *application_pb.Runtime) (*subscriptionPlan, error) {
+
+	plan := &subscriptionPlan{
+		ingressEndpoints: make(map[string]struct{}),
+	}
+
+	rulesByEnv := map[string]*eventBusRules{}
+	localEnvRules := &eventBusRules{
+		sourceEnvName: "local",
+		sourceEnvRef:  cloudformation.Ref(EnvNameParameter),
+	}
+	rulesByEnv[""] = localEnvRules
+
+	for _, sub := range spec.Subscriptions {
+		if sub.TargetContainer == "" {
+			sub.TargetContainer = spec.Containers[0].Name
+		}
+		if sub.Port == 0 {
+			sub.Port = 8080
+		}
+
+		plan.ingressEndpoints[fmt.Sprintf("%s:%d", sub.TargetContainer, sub.Port)] = struct{}{}
+
+		if strings.HasPrefix(sub.Name, "o5-infra/") {
+			topicName := sub.Name[len("o5-infra/"):]
+			if !spec.GrantMetaDeployPermissions {
+				return nil, fmt.Errorf("o5-infra subscription requires meta deploy permissions")
+			}
+			snsTopicARN := cloudformation.Join("", []string{
+				"arn:aws:sns:",
+				cloudformation.Ref("AWS::Region"),
+				":",
+				cloudformation.Ref("AWS::AccountId"),
+				":",
+				// Note this is the Cluster name, not the env name, one o5 listener per cluster.
+				cloudformation.Ref(ClusterNameParameter),
+				"-",
+				topicName,
+			})
+			plan.snsSubscriptions = append(plan.snsSubscriptions, snsSubscriptionPlan{
+				topicARN: snsTopicARN,
+				name:     topicName,
+			})
+			continue
+		}
+
+		var ruleSet *eventBusRules
+		if sub.EnvName == nil {
+			ruleSet = localEnvRules
+		} else {
+			var ok bool
+			ruleSet, ok = rulesByEnv[*sub.EnvName]
+			if !ok {
+				ruleSet = &eventBusRules{}
+				rulesByEnv[*sub.EnvName] = ruleSet
+			}
+			envParamName := cf.CleanParameterName(*sub.EnvName, "FullName")
+			ruleSet.sourceEnvRef = cloudformation.Ref(envParamName)
+			ruleSet.sourceEnvName = *sub.EnvName
+
+			plan.parameters = append(plan.parameters, &awsdeployer_pb.Parameter{
+				Name: envParamName,
+				Type: "String",
+				Source: &awsdeployer_pb.ParameterSourceType{
+					Type: &awsdeployer_pb.ParameterSourceType_CrossEnvAttr_{
+						CrossEnvAttr: &awsdeployer_pb.ParameterSourceType_CrossEnvAttr{
+							EnvName: *sub.EnvName,
+							Attr:    awsdeployer_pb.EnvAttr_FULL_NAME,
+						},
+					},
+				},
+			})
+		}
+
+		topicParts := strings.Split(sub.Name, "/")
+
+		if len(topicParts) == 1 {
+			// topic-name
+			ruleSet.topics = append(ruleSet.topics, topicParts[0])
+		} else if len(topicParts) == 2 {
+			// /foo.bar.v1.FooTopic
+			ruleSet.services = append(ruleSet.services, topicParts[1])
+		} else if len(topicParts) == 3 {
+			// /foo.bar.v1.FooTopic/FooMethod
+			ruleSet.methods = append(ruleSet.methods, serviceAndMethod{
+				service: topicParts[1],
+				method:  topicParts[2],
+			})
+		} else {
+			return nil, fmt.Errorf("invalid topic name %s", sub.Name)
+		}
+
+	}
+
+	for envGroup, rules := range rulesByEnv {
+
+		rulePatterns := make([]singlePattern, 0)
+
+		if len(rules.topics) > 0 {
+			rulePatterns = append(rulePatterns, singlePattern{
+				DestinationTopic:  rules.topics,
+				SourceEnvironment: []string{rules.sourceEnvRef},
+			})
+		}
+
+		if len(rules.services) > 0 {
+			rulePatterns = append(rulePatterns, singlePattern{
+				GRPCService:       rules.services,
+				SourceEnvironment: []string{rules.sourceEnvRef},
+			})
+		}
+
+		if len(rules.methods) > 0 {
+			methodMatchers := make(map[string]singlePattern, 0)
+			for _, method := range rules.methods {
+				mm, ok := methodMatchers[method.service]
+				if !ok {
+					mm = singlePattern{
+						GRPCService:       []string{method.service},
+						GRPCMethod:        []string{},
+						SourceEnvironment: []string{rules.sourceEnvRef},
+					}
+				}
+				mm.GRPCMethod = append(mm.GRPCMethod, method.method)
+				methodMatchers[method.service] = mm
+			}
+			for _, mm := range methodMatchers {
+				rulePatterns = append(rulePatterns, mm)
+			}
+		}
+
+		if len(rulePatterns) == 0 {
+			continue
+		}
+
+		var detailPattern busDetailPattern
+		if len(rulePatterns) == 1 {
+			detailPattern.singlePattern = &rulePatterns[0]
+		} else {
+			detailPattern.Or = rulePatterns
+		}
+
+		plan.eventBusSubscriptions = append(plan.eventBusSubscriptions, eventBusSubscriptionPlan{
+			name: envGroup,
+			eventPattern: busPattern{
+				Detail: detailPattern,
+			},
+		})
+
+	}
+
+	return plan, nil
+}
---
+++ cf/app/subscriptions_test.go
@@ -0,0 +1,210 @@
+package app
+
+import (
+	"encoding/json"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/awslabs/goformation/v7/cloudformation"
+	"github.com/pentops/o5-go/application/v1/application_pb"
+)
+
+func TestEventBusRules(t *testing.T) {
+
+	var localEnvRef = cloudformation.Ref(EnvNameParameter)
+	for _, tc := range []struct {
+		name  string
+		want  []map[string]interface{}
+		input *application_pb.Runtime
+	}{{
+		name:  "no rules",
+		want:  nil,
+		input: &application_pb.Runtime{},
+	}, {
+		name: "one topic rule",
+		input: &application_pb.Runtime{
+			Subscriptions: []*application_pb.Subscription{{
+				Name: "test",
+			}},
+		},
+		want: []map[string]interface{}{{
+			"detail": map[string]interface{}{
+				"sourceEnvironment": []interface{}{localEnvRef},
+				"destinationTopic":  []interface{}{"test"},
+			},
+		}},
+	}, {
+		name: "two topic rules",
+		input: &application_pb.Runtime{
+			Subscriptions: []*application_pb.Subscription{{
+				Name: "test",
+			}, {
+				Name: "test2",
+			}},
+		},
+		want: []map[string]interface{}{{
+			"detail": map[string]interface{}{
+				"sourceEnvironment": []interface{}{localEnvRef},
+				"destinationTopic":  []interface{}{"test", "test2"},
+			},
+		}},
+	}, {
+		name: "one service rule",
+		input: &application_pb.Runtime{
+			Subscriptions: []*application_pb.Subscription{{
+				Name: "/foo.v1.Bar",
+			}},
+		},
+		want: []map[string]interface{}{{
+			"detail": map[string]interface{}{
+				"sourceEnvironment": []interface{}{localEnvRef},
+				"grpcService":       []interface{}{"foo.v1.Bar"},
+			},
+		}},
+	}, {
+		name: "one method rule",
+		input: &application_pb.Runtime{
+			Subscriptions: []*application_pb.Subscription{{
+				Name: "/foo.v1.Bar/Baz",
+			}},
+		},
+		want: []map[string]interface{}{{
+			"detail": map[string]interface{}{
+				"sourceEnvironment": []interface{}{localEnvRef},
+				"grpcService":       []interface{}{"foo.v1.Bar"},
+				"grpcMethod":        []interface{}{"Baz"},
+			},
+		}},
+	}, {
+		name: "group methods by service",
+		input: &application_pb.Runtime{
+			Subscriptions: []*application_pb.Subscription{{
+				Name: "/foo.v1.Bar/Baz",
+			}, {
+				Name: "/foo.v1.Bar/Qux",
+			}},
+		},
+		want: []map[string]interface{}{{
+			"detail": map[string]interface{}{
+				"sourceEnvironment": []interface{}{localEnvRef},
+				"grpcService":       []interface{}{"foo.v1.Bar"},
+				"grpcMethod":        []interface{}{"Baz", "Qux"},
+			},
+		}},
+	}, {
+		name: "one service and method rule",
+		input: &application_pb.Runtime{
+			Subscriptions: []*application_pb.Subscription{{
+				Name: "/foo.v1.Bar/Baz",
+			}, {
+				Name: "/foo.v1.Qux",
+			}},
+		},
+		want: []map[string]interface{}{{
+			"detail": map[string]interface{}{
+				"$or": []interface{}{
+					map[string]interface{}{
+						"sourceEnvironment": []interface{}{localEnvRef},
+						"grpcService":       []interface{}{"foo.v1.Qux"},
+					},
+					map[string]interface{}{
+						"sourceEnvironment": []interface{}{localEnvRef},
+						"grpcService":       []interface{}{"foo.v1.Bar"},
+						"grpcMethod":        []interface{}{"Baz"},
+					},
+				},
+			},
+		}},
+	}} {
+		t.Run(tc.name, func(t *testing.T) {
+			if len(tc.input.Containers) == 0 {
+				tc.input.Containers = []*application_pb.Container{{
+					Name: "default",
+				}}
+			}
+			got, err := buildSubscriptionPlan(tc.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if len(got.eventBusSubscriptions) != len(tc.want) {
+				t.Errorf("expected %d rules, got %d", len(tc.want), len(got.eventBusSubscriptions))
+			}
+			for idx := range tc.want {
+				if idx >= len(got.eventBusSubscriptions) {
+					t.Errorf("extra rule %d rules, got %d", len(tc.want), len(got.eventBusSubscriptions))
+					continue
+				}
+				gotAsJSON, err := json.Marshal(got.eventBusSubscriptions[idx].eventPattern)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				gotAsMap := map[string]interface{}{}
+				if err := json.Unmarshal(gotAsJSON, &gotAsMap); err != nil {
+					t.Fatal(err)
+				}
+
+				t.Log(string(gotAsJSON))
+				assertJSONMapEqual(t, []string{"$"}, tc.want[idx], gotAsMap)
+			}
+		})
+
+	}
+
+}
+
+func assertJSONEqual(t *testing.T, path []string, expected, got interface{}) {
+	switch expected := expected.(type) {
+	case []interface{}:
+		gotSlice, ok := got.([]interface{})
+		if !ok {
+			t.Errorf("at %s: expected slice, got %T", strings.Join(path, "."), got)
+		}
+		assertJSONArrayEqual(t, path, expected, gotSlice)
+	case map[string]interface{}:
+		gotMap, ok := got.(map[string]interface{})
+		if !ok {
+			t.Errorf("at %s: expected map, got %T", strings.Join(path, "."), got)
+		}
+		assertJSONMapEqual(t, path, expected, gotMap)
+	default:
+		if !reflect.DeepEqual(expected, got) {
+			t.Errorf("at %s: DEEP expected %#v (%T), got %#v (%T)", strings.Join(path, "."), expected, expected, got, got)
+		}
+	}
+}
+
+func assertJSONArrayEqual(t *testing.T, path []string, expected, got []interface{}) {
+	if len(expected) != len(got) {
+		t.Errorf("expected %d elements, got %d", len(expected), len(got))
+	}
+
+	for ii, val := range expected {
+		if ii >= len(got) {
+			t.Errorf("at %s: index %d out of range", strings.Join(path, "."), ii)
+			continue
+		}
+		gotVal := got[ii]
+		assertJSONEqual(t, append(path, strconv.Itoa(ii)), val, gotVal)
+	}
+}
+
+func assertJSONMapEqual(t *testing.T, path []string, expected, got map[string]interface{}) {
+	for k, v := range expected {
+		got, ok := got[k]
+		if !ok {
+			t.Errorf("at %s: key %q not found", strings.Join(path, "."), k)
+		}
+
+		assertJSONEqual(t, append(path, k), v, got)
+	}
+
+	for k := range got {
+		if _, ok := expected[k]; !ok {
+			t.Errorf("unexpected key %q at %s", k, strings.Join(path, "."))
+		}
+	}
+}
